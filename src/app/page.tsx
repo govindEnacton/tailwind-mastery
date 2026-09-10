6adf3b49261a7c505bd324d12bd4924fb1ab8185
Govind <gggupta@enacton.email>
2026-09-10 09:15:16 +0000

    made a component in which onClick it route to the children is has

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
+"use client";
+import DynamicRouteButton from "@/components/DynamicRouteButton";
 import { Button } from "../components/ui/button";
 
+
 export default function Home() {
+  const popup = (name: string) => {
+    console.log(`you've clicked on the the ${name}`)
+  }
+
+
   return (
     <>
-    <div>
-      Here a normal webpage will come for let the user see button for the form , kanban and builder like this 
-    </div>
-    <div>
-      <Button variant="default" size="default">
-        Form
-      </Button>
-      <Button variant="default" size="default">
-        Kanban
-      </Button>
-      <Button variant="default" size="default">
-        Builder
-      </Button>
+      <div>
+        Here a normal webpage will come for let the user see button for the form , kanban and builder like this
+      </div>
+      <div>
+        <DynamicRouteButton>Form </DynamicRouteButton>
+        <DynamicRouteButton>Kanban</DynamicRouteButton>
+        <DynamicRouteButton>Builder </DynamicRouteButton>
       </div>
     </>
   );
---
+++ src/components/DynamicRouteButton.tsx
@@ -0,0 +1,52 @@
+// "use client";
+//
+// import { Button, type ButtonProps } from "@/components/ui/button";
+// import { useRouter } from "next/navigation";
+//
+// type DynamicRouteButtonProps = ButtonProps & {
+//   route: string;
+// };
+//
+// export default function DynamicRouteButton({
+//   children,
+//   ...props
+// }: DynamicRouteButtonProps) {
+//   const router = useRouter();
+//
+//   const handleClick = () => {
+//     router.push(children);
+//   };
+//
+//   return (
+//     <Button {...props} onClick={handleClick}>
+//       {children}
+//     </Button>
+//   );
+// }
+
+"use client";
+
+import { Button, type ButtonProps } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+type DynamicRouteButtonProps = Omit<ButtonProps, "children"> & {
+  children: string;
+};
+
+export default function DynamicRouteButton({
+  children,
+  ...props
+}: DynamicRouteButtonProps) {
+const routeChildren = children.toLowerCase();
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(routeChildren);
+  };
+
+  return (
+    <Button {...props} onClick={handleClick}>
+      {children}
+    </Button>
+  );
+}
