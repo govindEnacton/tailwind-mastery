9ebaa1ce797c7ef04362b4d987b8cdb4fe1e0d40
Govind <gggupta@enacton.email>
2026-09-10 09:16:04 +0000

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-"use client";
 import DynamicRouteButton from "@/components/DynamicRouteButton";
-import { Button } from "../components/ui/button";
-
 
 export default function Home() {
-  const popup = (name: string) => {
-    console.log(`you've clicked on the the ${name}`)
-  }
-
 
   return (
     <>
-      <div>
-        Here a normal webpage will come for let the user see button for the form , kanban and builder like this
-      </div>
       <div>
         <DynamicRouteButton>Form </DynamicRouteButton>
         <DynamicRouteButton>Kanban</DynamicRouteButton>
